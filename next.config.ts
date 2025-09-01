5c94521a0cfb9b3e797950cba16a6403e38e5b14
Peter Beverloo <pbeverloo@gmail.com>
2025-09-01 21:57:22 +0100

    faff: next.config.js --> next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 // Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-const nextBuildId = require('next-build-id');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: true });
+import type { NextConfig } from 'next';
+import NextBundleAnalyzer from '@next/bundle-analyzer';
+import nextBuildId from 'next-build-id';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
     env: {
         // `process.env.GIT_COMMIT` will be set for Docker builds, where it's determined through the
         // next-build-id library locally on the machine for other kinds of builds. Do update
@@ -30,11 +30,12 @@ const nextConfig = {
             permanent: true,
         },
     ]),
+    typedRoutes: false,
     typescript: {
         tsconfigPath: './tsconfig.build.json',
     },
 };
 
 module.exports = process.env.ANALYZE === 'true'
-    ? withBundleAnalyzer(nextConfig)
+    ? NextBundleAnalyzer({ enabled: true })(nextConfig)
     : nextConfig;
