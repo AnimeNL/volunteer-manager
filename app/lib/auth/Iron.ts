0e73c379ca0e4187a4ea01b7f0757b1fb4e55e2e
Peter Beverloo <pbeverloo@gmail.com>
2025-12-01 12:26:22 +0000

    faff: Replace iron-webcrypto with an extracted implementation
    They released a new major that's causing all sorts of weird failures, and this is straightforward enough to extract in a file of our own. (Yes, while acknowledging it's crypto stuff.)

--- app/lib/auth/Iron.ts
+++ app/lib/auth/Iron.ts
@@ -1,7 +1,35 @@
 // Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import * as Iron from 'iron-webcrypto';
+/**
+ * This implementation is extracted from iron-webcrypto, which in itself is based on @hapi/iron.
+ * Initially we benefitted from a WebCrypto fallback, but with modern versions of Node.js this no
+ * longer is required, and we can implement the data marshalling to WebCrypto ourselves.
+ *
+ * @see https://www.npmjs.com/package/iron-webcrypto
+ * @see https://www.npmjs.com/package/@hapi/iron
+ */
+
+// -------------------------------------------------------------------------------------------------
+
+/**
+ * Prefix indicating that this is our packaging format.
+ */
+const kPrefix = 'Fe26.2';
+
+/**
+ * Utilities to convert between Uint8Array instances and base64/hex strings.
+ * @todo Adopt https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toHex
+ */
+const base64ToUint8Array = (data: string) => Buffer.from(data, 'base64');
+const uint8ArrayToBase64 = (data: Uint8Array<ArrayBuffer>) => Buffer.from(data).toString('base64');
+const uint8ArrayToHex = (data: Uint8Array<ArrayBuffer>) => Buffer.from(data).toString('hex');
+
+/**
+ * File-global instances of the default text encoder and decoder.
+ */
+const kTextEncoder = new TextEncoder();
+const kTextDecoder = new TextDecoder();
 
 /**
  * Seals the given `data` using the `password`. The sealed data will be valid for, at most, the
@@ -16,11 +44,33 @@ export async function seal(data: unknown, password: string, ttl: number): Promis
     if (password.length < 32)
         throw new Error('The password used to seal data must be at least 32 characters long.');
 
-    // @ts-ignore: ArrayBuffer -> ArrayBufferLike migration stuff
-    return Iron.seal(globalThis.crypto, data, password, {
-        ...Iron.defaults,
-        ttl: ttl * /* milliseconds= */ 1000,
-    });
+    const encodedData = kTextEncoder.encode(JSON.stringify(data));
+
+    const key = await generateKey('aes-256-cbc', password);
+    const iv = await generateBits(/* ivBits= */ 128);
+
+    const encrypted = await crypto.subtle.encrypt({ name: 'AES-CBC', iv }, key.key, encodedData);
+
+    const macBaseString = [
+        kPrefix,
+        /* password id= */ '',
+        /* encryption salt= */ key.salt,
+        /* encryption iv= */ uint8ArrayToBase64(iv),
+        /* encryption data= */ uint8ArrayToBase64(new Uint8Array(encrypted)),
+        /* expiration time= */ Date.now() + ttl * 1000,
+
+    ].join('*');
+
+    const integrityKey = await generateKey('sha-256', password);
+    const integritySignature =
+        await crypto.subtle.sign('HMAC', integrityKey.key, kTextEncoder.encode(macBaseString));
+
+    return [
+        macBaseString,
+        /* integrity salt= */ integrityKey.salt,
+        /* integrity digest= */ uint8ArrayToBase64(new Uint8Array(integritySignature)),
+
+    ].join('*');
 }
 
 /**
@@ -36,9 +86,111 @@ export async function unseal(sealedData: string, password: string, ttl: number):
     if (password.length < 32)
         throw new Error('The password used to unseal data must be at least 32 characters long.');
 
-    // @ts-ignore: ArrayBuffer -> ArrayBufferLike migration stuff
-    return Iron.unseal(globalThis.crypto, sealedData, password, {
-        ...Iron.defaults,
-        ttl: ttl * /* milliseconds= */ 1000,
-    });
+    const parts = sealedData.split('*');
+    if (parts.length !== 8)
+        throw new Error(`Invalid number of sealed components (expected 8, got ${parts.length})`);
+
+    const [ prefix, _, encryptionSalt, encryptionIv, encryptionData, expiration, integritySalt,
+        integrityDigest ] = parts;
+
+    // (1) Prefix validation
+    if (prefix !== kPrefix)
+        throw new Error(`Invalid prefix (expected "${kPrefix}", got "${prefix}")`);
+
+    // (2) Expiration time validation
+    {
+        if (!/^[1-9]\d*$/.test(expiration))
+            throw new Error(`Invalid expiration time (expected a number, got "${expiration})`);
+
+        const expirationTime = parseInt(expiration, /* radix= */ 10);
+        if (expirationTime < Date.now())
+            throw new Error(`Invalid expiration time (in the past)`);
+    }
+
+    // (3) Verify the signature
+    {
+        const key = await generateKey('sha-256', password, integritySalt);
+        const macBaseString = parts.slice(0, 6).join('*');
+
+        const verify = await crypto.subtle.verify(
+            'HMAC', key.key, base64ToUint8Array(integrityDigest),
+            kTextEncoder.encode(macBaseString));
+
+        if (!verify)
+            throw new Error(`Invalid HMAC integrity digest, seal has been broken`);
+    }
+
+    // (4) Decrypt the data
+    const key = await generateKey('aes-256-cbc', password, encryptionSalt);
+    const decrypted = await crypto.subtle.decrypt({
+        name: 'AES-CBC',
+        iv: base64ToUint8Array(encryptionIv),
+    }, key.key, base64ToUint8Array(encryptionData));
+
+    const decryptedString = kTextDecoder.decode(decrypted);
+    return JSON.parse(decryptedString);
+}
+
+/**
+ * Generates cryptographically strong pseudorandom bits.
+ *
+ * @param bits Number of bits to generate (must be a positive multiple of 8).
+ * @returns Uint8Array containing the random bits.
+ */
+async function generateBits(bits: number) {
+    return crypto.getRandomValues(new Uint8Array(bits / 8));
+}
+
+/**
+ * Generates a key based on the given |password|, using |type| to deriviate it.
+ *
+ * @param type Type of derived key to generate.
+ * @param password The password string.
+ * @param salt The salt to use. Optional.
+ * @returns The derived key and the used salt.
+ */
+async function generateKey(type: 'aes-256-cbc' | 'sha-256', password: string, useSalt?: string) {
+    const encodedPassword = kTextEncoder.encode(password);
+    const salt = useSalt ?? uint8ArrayToHex(await generateBits(/* saltBits= */ 256));
+
+    const algorithm = {
+        name: 'PBKDF2',
+        salt: kTextEncoder.encode(salt),
+        iterations: 1,
+        hash: 'SHA-1'
+    };
+
+    let derivedKeyUsage: KeyUsage[];
+    let derivedKeyParams: AesDerivedKeyParams | HmacImportParams;
+
+    switch (type) {
+        case 'aes-256-cbc':
+            derivedKeyUsage = [ 'encrypt', 'decrypt' ];
+            derivedKeyParams = {
+                name: 'AES-CBC',
+                length: 256
+            };
+            break;
+
+        case 'sha-256':
+            derivedKeyUsage = [ 'sign', 'verify' ];
+            derivedKeyParams = {
+                name: 'HMAC',
+                hash: 'SHA-256',
+                length: 256
+            };
+            break;
+
+        default:
+            throw new Error(`Invalid derived key type: ${type}`);
+    }
+
+    const baseKey =
+        await crypto.subtle.importKey('raw', encodedPassword, 'PBKDF2', false, [ 'deriveKey' ]);
+
+    const derivedKey =
+        await crypto.subtle.deriveKey(
+            algorithm, baseKey, derivedKeyParams, /* extractable= */ false, derivedKeyUsage);
+
+    return { key: derivedKey, salt };
 }
