2756d9f01cb3a94fd3c74f600980316f887bd4bf
Peter Beverloo <pbeverloo@gmail.com>
2026-01-26 23:09:23 +0000

    fix: Use the right HTTP status code when accessing an inaccessible event

--- app/registration/[slug]/layout.tsx
+++ app/registration/[slug]/layout.tsx
@@ -1,7 +1,7 @@
 // Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import { notFound, unauthorized } from 'next/navigation';
+import { forbidden, notFound, unauthorized } from 'next/navigation';
 
 import { ApplicationProgressHeader } from '@app/welcome/ApplicationProgressHeader';
 import { RegistrationContentContainer } from '../RegistrationContentContainer';
@@ -31,8 +31,10 @@ export default async function RegistrationEventLayout(props: LayoutProps<'/regis
                 access = true;
         }
 
-        if (!access)
-            unauthorized();
+        if (!access) {
+            !!context.user ? forbidden()
+                           : unauthorized();
+        }
 
         event = eventCandidate;
         break;
