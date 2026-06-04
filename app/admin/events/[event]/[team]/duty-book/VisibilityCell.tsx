1d08b60784dd510d9d05cd86c9f9b9d943a5bba4
Peter Beverloo <pbeverloo@gmail.com>
2026-06-04 22:12:16 +0100

    faff: Migrate the Duty Book overview page to the new `<DataTable>`

---
+++ app/admin/events/[event]/[team]/duty-book/VisibilityCell.tsx
@@ -0,0 +1,40 @@
+// Copyright 2026 Peter Beverloo & AnimeCon. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+'use client';
+
+import Tooltip from '@mui/material/Tooltip';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+import type { DutyBookRowModel } from './page';
+
+/**
+ * Header component for the visibility column.
+ */
+export function VisibilityHeaderCell() {
+    return (
+        <Tooltip title="Are the details available?">
+            <VisibilityIcon fontSize="small" color="primary" />
+        </Tooltip>
+    );
+}
+
+/**
+ * Cell component for the visibility status.
+ */
+export function VisibilityCell({ row }: { row: DutyBookRowModel }) {
+    if (row.hidden) {
+        return (
+            <Tooltip title="Details have been hidden">
+                <VisibilityOffIcon fontSize="small" color="error" />
+            </Tooltip>
+        );
+    } else {
+        return (
+            <Tooltip title="Details are available">
+                <VisibilityIcon fontSize="small" color="success" />
+            </Tooltip>
+        );
+    }
+}
